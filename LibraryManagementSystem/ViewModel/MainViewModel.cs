07ba3529f395ee6a4c1ac1757f64a4c4591c711b
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2023-04-09 20:00:45 +0700

    Create Base WPF application for our Project

---
+++ LibraryManagementSystem/ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LibraryManagementSystem.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+        #region Property
+
+        #endregion
+
+        #region ICommand
+        public ICommand Login { get; set; }
+        #endregion
+        public MainViewModel()
+        {
+            Login = new RelayCommand<Frame>((p) => { return true; }, async (p) =>
+            {
+                MessageBox.Show("Loo cc");
+            });
+        }
+    }
+}
