5e4b9240ddfa7057076d8ea6f61c07166517dc1f
Nguyễn Hoàng Minh <109229679+hoangmindrespect@users.noreply.github.com>
2023-04-15 10:34:59 +0700

    login + regis + custom messbox

---
+++ LibraryManagementSystem/View/Login/loginwindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryManagementSystem.View.Login
+{
+    /// <summary>
+    /// Interaction logic for loginwindow.xaml
+    /// </summary>
+    public partial class loginwindow : Window
+    {
+        public static Card a;
+
+        public loginwindow()
+        {
+            InitializeComponent();
+            a = blur_card;
+        }
+
+        private void btnMinimizeMouseEnter(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFA5B9D6");
+        }
+
+        private void btnMinimizeMouseLeave(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = new SolidColorBrush(Colors.White);
+        }
+
+        private void btnCloseMouseEnter(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+
+            btn.Background = (SolidColorBrush)new BrushConverter().ConvertFrom("#FFA5B9D6");
+            btn.Background = new SolidColorBrush(Colors.OrangeRed);
+        }
+
+        private void btnCloseMouseLeave(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Background = new SolidColorBrush(Colors.White);
+        }
+
+        private void btnMinimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnClose(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
